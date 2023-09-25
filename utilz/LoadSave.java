5eb672b1a8f5229aa7d026f0047d2318f0e78eda
chulan13 <105126514+chulan13@users.noreply.github.com>
2023-09-25 14:48:34 +0200

    Create LoadSave.java

---
+++ utilz/LoadSave.java
@@ -0,0 +1,27 @@
+package utilz;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class LoadSave {
+    public static final String PlAYER_ATLAS = "cringe_cat.png";
+    public static BufferedImage GetSpriteAtlas(String fileName){
+        BufferedImage img = null;
+        InputStream is = LoadSave.class.getResourceAsStream("/" + fileName);
+        try {
+
+            img = ImageIO.read(is);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return img;
+    }
+}
