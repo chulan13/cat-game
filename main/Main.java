0cbaa753d47c7bc8366d0fa267b5019c7568021c
chulan13 <105126514+chulan13@users.noreply.github.com>
2023-09-25 14:44:09 +0200

    Create Main.java

---
+++ main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+
+// Press ⇧ twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        new Game();
+    }
+}
